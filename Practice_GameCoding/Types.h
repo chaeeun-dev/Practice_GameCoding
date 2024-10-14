626263faeb7dc37d57872412bae49f32a033e437
Im Chaeeun <dev.chaeng@gmail.com>
2024-10-14 23:24:08 +0900

    내적, 외적까지

--- Practice_GameCoding/Types.h
+++ Practice_GameCoding/Types.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <cmath>
+#include <windows.h>
+
 // 변수를 편리하게 사용하기 위해
 
 using int8 = __int8;
@@ -24,3 +27,93 @@ struct Pos
 	float y = 0;
 };
 
+struct Vector
+{
+	Vector() {}
+	Vector(int32 x, int32 y) : x(x), y(y) {}
+	Vector(POINT pt) : x((float)pt.x), y((float)pt.y) {}
+
+	// Operator 
+	Vector operator+(const Vector& other)
+	{
+		Vector ret;
+		ret.x = x + other.x;
+		ret.y = y + other.y;
+		return ret;
+	}
+
+	Vector operator-(const Vector& other)
+	{
+		Vector ret;
+		ret.x = x - other.x;
+		ret.y = y - other.y;
+		return ret;
+	}
+		
+	Vector operator*(float value)
+	{
+		Vector ret;
+		ret.x = x * value;
+		ret.y = y * value;
+		return ret;
+	}
+		
+	Vector operator+=(const Vector& other)
+	{
+		Vector ret;
+		x += other.x;
+		y += other.y;
+		return ret;
+	}
+
+	Vector operator-=(const Vector& other)
+	{
+		Vector ret;
+		x -= other.x;
+		y -= other.y;
+		return ret;
+	}
+		
+	Vector operator*=(float ratio)
+	{
+		Vector ret;
+		x *= ratio;
+		y *= ratio;
+		return ret;
+	}
+
+	float LengthSquared()
+	{
+		return x * x + y * y;
+	}
+
+	float Length()
+	{
+		return ::sqrt(LengthSquared());
+	}
+
+	void Normalize()
+	{
+		float length = Length();
+		if (length < 0.00000000001f)
+			return;
+
+		x /= length;
+		y /= length;
+	}
+
+	float Dot(Vector other)
+	{
+		return x * other.x + y * other.y;
+	}
+
+	float Cross(Vector other)
+	{
+		return x * other.y - y * other.x;
+	}
+
+	float x = 0;
+	float y = 0;
+};
+
+using Pos = Vector;
