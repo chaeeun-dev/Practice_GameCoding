eca12fd4498df73cbe2f27deebcc21191b6d15cc
Im Chaeeun <dev.chaeng@gmail.com>
2024-10-20 00:35:35 +0900

    Flipbook class 생성 후 ResourceManager에 추가

--- Practice_GameCoding/ResourceManager.h
+++ Practice_GameCoding/ResourceManager.h
@@ -4,6 +4,7 @@
 class ResourceBase;
 class Texture;
 class Sprite;
+class Flipbook;
 
 class ResourceManager
 {
@@ -27,6 +28,10 @@ public:
 	Sprite* CreateSprite(const wstring& key, Texture* texture, int32 x = 0, int32 y = 0, int32 cx = 0, int32 cy = 0);
 	// Load된 Texure를 가져와 사용하기 때문에 Load가 아닌 Create
 
+	// Flipbook
+	Flipbook* GetFlipbook(const wstring& key) { return _flipbooks[key]; }
+	Flipbook* CreateFlipbook(const wstring& key);
+
 private:
 	HWND _hwnd;		// 도화지 번호
 	std::filesystem::path _resourcePath;	// 리소스 경로
@@ -34,5 +39,6 @@ private:
 	// resource
 	unordered_map<wstring, Texture*> _textures;
 	unordered_map<wstring, Sprite*> _sprites;
+	unordered_map<wstring, Flipbook*> _flipbooks;
 };
 
